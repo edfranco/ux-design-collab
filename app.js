d2ed97ae6079dcc45c962084e64a123b5254f4fd
edfranco <edfranco415@gmail.com>
2019-08-01 16:42:12 -0700

    added sidenav

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const input;
-if (input.contains('@') && input.contains('.')) {
+// const input;
+// if (input.contains('@') && input.contains('.')) {
 
-} else {
-    console.log('Please input a valid email');
+// } else {
+//     console.log('Please input a valid email');
+// }
+
+// if(input.innerHTML = ''){
+//     console.log('Please input something')
+// }
+
+//Const Variables 
+const navButton = document.getElementById('navButton');
+const sideNav = document.getElementById('sideNav');
+
+
+// Functions
+const dropNav = (event) => {
+    sideNav.style.width = "50vw";
 }
+
+navButton.addEventListener('click', dropNav, false)
